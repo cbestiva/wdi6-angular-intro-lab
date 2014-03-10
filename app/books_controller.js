9a44e33c2ef0b04eb48bcc09f700bddc2097573f
cbestiva <cbestiva@gmail.com>
2014-03-10 15:18:53 -0700

    added search box and drop down

--- app/books_controller.js
+++ app/books_controller.js
@@ -6,6 +6,11 @@ bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
 
+  $scope.addBook = function(book) {
+    $scope.books.push(book);
+    $scope.book ={};
+  };
+
   //Code to manage cart goes here
 
 };
