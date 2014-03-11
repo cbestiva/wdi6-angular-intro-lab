7b0efbce4261b5dca7c804f1dfe54ce27f8e294c
cbestiva <cbestiva@gmail.com>
2014-03-10 17:52:32 -0700

    can get total cart value

--- app/books_controller.js
+++ app/books_controller.js
@@ -12,7 +12,13 @@ bookly.BooksController = function($scope) {
     $scope.cart.push(book);
   };
 
-  
+  $scope.totalPrice = function() {
+    var total = 0;
+    for (i=0; i<$scope.cart.length;i++) {
+      total += $scope.cart[i].price;
+    }
+    return total;
+  };
 
 
 };
