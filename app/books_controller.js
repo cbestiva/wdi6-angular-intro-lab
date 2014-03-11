9a0b329307df20f4e7e3773ccb49c21cda433dc5
cbestiva <cbestiva@gmail.com>
2014-03-11 10:02:42 -0700

    added quantity and updated totalPrice function for each book

--- app/books_controller.js
+++ app/books_controller.js
@@ -8,14 +8,26 @@ bookly.BooksController = function($scope) {
 
   //Code to manage cart goes here
   $scope.cart = [];
-  $scope.addToCart = function(book) {
-    $scope.cart.push(book);
+  // $scope.qty = 0;
+
+  $scope.addToCart = function(index) {
+    var cart = $scope.cart;
+    var book = $scope.books[index];
+    if (cart.indexOf(book) >= 0) {
+      cart[cart.indexOf(book)].count += 1;
+    }
+    else {
+      cart.push(book);
+      cart[cart.indexOf(book)].count = 1;
+    }
+    // $scope.cart.push(book);
+    // $scope.qty += 1;
   };
 
   $scope.totalPrice = function() {
     var total = 0;
     for (i=0;i<$scope.cart.length;i++) {
-      total += $scope.cart[i].price;
+      total += ($scope.cart[i].price) * ($scope.cart[i].count);
     }
     return total;
   };
@@ -23,6 +35,7 @@ bookly.BooksController = function($scope) {
   $scope.emptyCart = function() {
     $scope.cart = [];
   };
+
 
 
 };
